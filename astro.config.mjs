011e374dfdfd5c72e2de0680424cc28c63f43cf5
vasyaqwe <elekton.fafik@gmail.com>
2024-11-23 09:06:07 +0200

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// import sitemap from "@astrojs/sitemap"
+import { defineConfig } from "astro/config"
+
+// https://astro.build/config
+export default defineConfig({
+   // site: "https://",
+   integrations: [],
+   server: {
+      port: 3000,
+   },
+})
